1840bf27daf5c25c060cf39747e151039a93c073
dhivya-ait <85148572+Dhivya-123maker@users.noreply.github.com>
2022-06-21 16:54:10 +0530

    change, 21

--- app/src/main/java/com/example/shopkeeper/Cart/FragmentCart.java
+++ app/src/main/java/com/example/shopkeeper/Cart/FragmentCart.java
@@ -1,18 +1,18 @@
 package com.example.shopkeeper.Cart;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.shopkeeper.R;
+import com.google.android.filament.View;
 
 import java.util.ArrayList;
 import java.util.List;
